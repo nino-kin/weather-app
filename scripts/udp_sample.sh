edc2238e4121673a5f1cab4e999631146f9c32e6
Masa <64725574+masa25hw@users.noreply.github.com>
2023-09-20 01:22:58 +0900

    Get weather data (#8)
    * feat: Get weather data (OpenWeather)
    
    We can get today's weather data in Tokyo.
    This feature uses OpenWeather API (One Call API 3.0) to get the weather
    data. If you want to know it, you should create your account of
    OpenWeather and get your API.
    
    https://openweathermap.org/api/one-call-3
    
    This separate subscription includes 1,000 calls/day for free and allows
    you to pay only for the number of API calls made to this product.
    Once you subscribe to One call API 3.0, 2000 API calls per day to this
    product are set up by default. If you want to change this limit, please
    go to the "Billing plans" tab in your Personal account to update
    standard settings.
    
    * build: Update build environment (Docker, CMake)
    
    * feat: Formatting stdout info
    
    Formatting the output of the weather data to use the `fmt` library.
    
    * feat: Change temperature unit (fahrenheit->celsius)
    
    The default temperature unit is `Fahrenheit`.
    By adding `&units=metric` to the end of the URL(API call),
    the temperature can be responded in `Celsius` units.
    
    * build: Setup build environment for `fmt`
    
    * fix: Remove build artifacts forcibly (sudo)
    
    * feat: Helper script for Docker
    
    * refactor: Change the path of the script to call
    
    * refactor: Change the stdout message
    
    * refactor: Remove unnecessary steps
    
    The build process was divided as follows:
    
    - Build with CMake
    - Build and deploy docs with MkDocs
    
    * feat: Build with CMake in Docker container
    
    * test: Build workflow
    
    * refactor: Change the message
    
    * fix: Fixed docker option
    
    Remove `-i` option.

--- scripts/udp_sample.sh
+++ scripts/udp_sample.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+REPO_ROOT_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
+
 # Run server
-./build/src/sample/server/udp_server &
+$REPO_ROOT_DIR/build/src/sample/server/udp_server &
 
 # Run client
 sleep 1
-./build/src/sample/client/udp_client
+$REPO_ROOT_DIR/build/src/sample/client/udp_client
 
 wait
---
+++ src/weather_service/weather_fetcher.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <curl/curl.h>
+#include <fmt/core.h>
+#include <nlohmann/json.hpp>
+
+const std::string CITY_NAME = "Tokyo";
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
+    size_t totalSize = size * nmemb;
+    output->append(static_cast<char*>(contents), totalSize);
+    return totalSize;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <API_KEY>" << std::endl;
+        return 1;
+    }
+
+    const std::string API_KEY = argv[1];
+    CURL* curl;
+    CURLcode res;
+    std::string apiResponse;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl) {
+        std::string apiKey = API_KEY;
+        std::string city = CITY_NAME;
+
+        std::string apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+
+        curl_easy_setopt(curl, CURLOPT_URL, apiUrl.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &apiResponse);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        } else {
+            // Parse JSON
+            nlohmann::json jsonData = nlohmann::json::parse(apiResponse);
+
+            // Get weather information
+            std::string weatherDescription = jsonData["weather"][0]["description"];
+            double temperature = jsonData["main"]["temp"];
+            int humidity = jsonData["main"]["humidity"];
+
+            fmt::print("City       : {}\n", city);
+            fmt::print("Weather    : {}\n", weatherDescription);
+            fmt::print("Temperature: {:.2f} °C\n", temperature);
+            fmt::print("Humidity   : {}%\n", humidity);
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+
+    return 0;
+}
