956dfffc7eb0267760710a5c0a9ba3963642b3f8
nico <64725574+nico25-orange@users.noreply.github.com>
2023-12-09 23:40:26 +0900

    Weather app with Javascript (#29)
    * refactor: remove existing functions
    
    * refactor: rename pull pull request template
    
    * docs: add images
    
    * chore: change code owners
    
    * feat: add weather app with javascript
    
    * refactor: remove unnecessary lines
    
    * refactor: remove main workflow

--- include/udp_common.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef NINOKIN_INCLUDE_UDP_COMMON_HPP_
-#define NINOKIN_INCLUDE_UDP_COMMON_HPP_
-
-// UDP configuration
-#define PORT     8080
-#define MAXLINE 1024
-
-#endif // NINOKIN_INCLUDE_UDP_COMMON_HPP_
